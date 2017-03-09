fe6cc86fb1a6992d0fac1423fe9027a860ee0391
Tomohiro Mitsusmune <tmitsumune@gmail.com>
2017-03-09 18:30:12 +0900

    Create morph package

--- ch05/040/main.go
+++ ch05/040/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+
+	"github.com/tmitz/nlp100-go/ch05/040/morph"
 )
-
-type Morph struct { // nolint
-	Surface, Base, Pos, Pos1 string
-}
-
-type Morphs []Morph // nolint
 
 func main() {
 	file := os.Args[1:]
-	f, err := os.Open(file[0])
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	sc := bufio.NewScanner(f)
-	ms := make(Morphs, 0)
-	for sc.Scan() {
-		text := sc.Text()
-		if strings.HasPrefix(text, "*") || strings.Contains(text, "EOS") {
-			continue
-		}
-		line := strings.Split(text, "\t")
-		parse := strings.Split(line[1], ",")
-		ms = append(ms, Morph{Surface: line[0], Base: parse[6], Pos: parse[0], Pos1: parse[1]})
-	}
-	fmt.Println(ms[2])
+	res := morph.List(file[0])
+	fmt.Println(res[2])
 }
