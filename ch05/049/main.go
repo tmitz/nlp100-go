bd7012a9ddc4d95d060a60a864e7673f9a6ffe36
Tomohiro Mitsusmune <tmitsumune@gmail.com>
2017-03-23 13:09:26 +0900

    Fix ch05/049

--- ch05/049/main.go
+++ ch05/049/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"strings"
+
 	"github.com/tmitz/nlp100-go/ch05/041/chunk"
 )
 
@@ -11,31 +13,40 @@ type NounPair []chunk.Chunk
 type NounPairs []NounPair
 
 func main() {
-	file := os.Args[1:]
-	sentences := chunk.List(file[0])
+	file := os.Args[1:][0]
+	sentences := chunk.List(file)
 
-	for _, sentence := range sentences {
+	for _, sentence := range sentences[:100] {
 		npairs := nounPairs(sentence)
 		if len(npairs) == 0 {
 			continue
 		}
+		var formerRoot chunk.Sentence
+		var latterRoot chunk.Sentence
+		for _, pair := range npairs {
+			former, latter := pair[0], pair[1]
+			former.ReplaceNoun("X")
+			latter.ReplaceNoun("Y")
 
-		var pathciRoot chunk.Sentence
-		// var pathcjRoot chunk.Sentence
-		for _, n := range npairs {
-			ci, cj := n[0], n[1]
-			ci.ReplaceNoun("X")
-			cj.ReplaceNoun("Y")
+			formerRoot = pathToRoot(former, sentence)
+			latterRoot = pathToRoot(latter, sentence)
 
-			pathciRoot = pathToRoot(ci, sentence)
-			pathcjRoot = pathToRoot(cj, sentence)
-
-			fmt.Println(cj)
-			fmt.Println(pathciRoot)
-			if containNounChunk(cj, pathciRoot) {
-				fmt.Println("unko")
+			latterX := latter
+			latterX.ReplaceNoun("X")
+			if containNounChunk(latterX, formerRoot) {
+				idx := findRootIndex(latterX, formerRoot)
+				fmt.Print(joinChunksArrow(formerRoot[:idx+1]))
 			} else {
-				// fmt.Println(pathcjRoot)
+				common := commonChunk(formerRoot, latterRoot)
+				if common.Dst == 0 && common.Srcs == 0 {
+					continue
+				}
+				idxf := findRootIndex(common, formerRoot)
+				idxl := findRootIndex(common, latterRoot)
+
+				var path []string
+				path = append(path, joinChunksArrow(formerRoot[:idxf]), joinChunksArrow(latterRoot[:idxl]), fmt.Sprint(common))
+				fmt.Println(strings.Join(path, " | "))
 			}
 		}
 	}
@@ -50,6 +61,14 @@ func pathToRoot(c chunk.Chunk, s chunk.Sentence) chunk.Sentence {
 		res = append(res, pathToRoot(s[c.Dst], s)...)
 	}
 	return res
+}
+
+func joinChunksArrow(chunks []chunk.Chunk) string {
+	var res []string
+	for _, c := range chunks {
+		res = append(res, fmt.Sprint(c))
+	}
+	return strings.Join(res, " -> ")
 }
 
 func nounPairs(sentence chunk.Sentence) NounPairs {
@@ -69,6 +88,43 @@ func nounPairs(sentence chunk.Sentence) NounPairs {
 	}
 
 	return nounPairs
+}
+
+func commonChunk(former chunk.Sentence, latter chunk.Sentence) chunk.Chunk {
+	ch := chunk.Chunk{}
+	rformer := reverseRoot(former)
+	rlatter := reverseRoot(latter)
+
+	for i, cf := range rformer {
+		if i > len(rlatter)-1 {
+			continue
+		}
+		cl := rlatter[i]
+		if cf.Srcs != cl.Srcs {
+			ch = rformer[i-1]
+			break
+		}
+	}
+
+	return ch
+}
+
+func findRootIndex(c chunk.Chunk, root chunk.Sentence) int {
+	idx := -1
+	for i, v := range root {
+		if fmt.Sprint(v) == fmt.Sprint(c) {
+			idx = i
+		}
+	}
+	return idx
+}
+
+func reverseRoot(root chunk.Sentence) chunk.Sentence {
+	var reverse chunk.Sentence
+	for i := len(root) - 1; i > -1; i-- {
+		reverse = append(reverse, root[i])
+	}
+	return reverse
 }
 
 func containNounChunk(c chunk.Chunk, s chunk.Sentence) bool {
